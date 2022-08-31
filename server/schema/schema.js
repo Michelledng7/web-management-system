1ad3df7d8d8004dd9fea89d9313f1b65ed471f9e
Michelle <michelledng7@gmail.com>
2022-09-01 00:30:11 +1000

    add mutation for delete and update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -136,6 +136,52 @@ const mutation = new GraphQLObjectType({
 				return project.save();
 			},
 		},
+
+		//delete project
+		deleteProject: {
+			type: ProjectType,
+			args: {
+				id: { type: GraphQLNonNull(GraphQLID) },
+			},
+			resolve(parent, args) {
+				return Project.findByIdAndRemove(args.id);
+			},
+		},
+
+		//update project
+		updateProject: {
+			type: ProjectType,
+			args: {
+				id: { type: GraphQLNonNull(GraphQLID) },
+				name: { type: GraphQLString },
+				description: { type: GraphQLString },
+				status: {
+					type: new GraphQLEnumType({
+						name: 'ProjectStatusUpdate',
+						values: {
+							new: { value: 'Not Started' },
+							progress: { value: 'In Progress' },
+							completed: { value: 'Completed' },
+						},
+					}),
+				},
+				clientId: { type: GraphQLNonNull(GraphQLID) },
+			},
+			resolve(parent, args) {
+				return Project.findByIdAndUpdate(
+					args.id,
+					{
+						$set: {
+							name: args.name,
+							description: args.description,
+							status: args.status,
+							//clientId: args.clientId,
+						},
+					},
+					{ new: true }
+				);
+			},
+		},
 	},
 });
 
